5f357d3abe50ed47f4abed55184bd955cc8623d7
tuan.nguyen <winqti@gmail>
2025-08-19 23:16:41 +0700

    feat: add page struct

--- internal/storage/page/page.go
+++ internal/storage/page/page.go
@@ -1,1 +1,18 @@
 package page
+
+import util "github.com/bietkhonhungvandi212/array-db/internal/utils"
+
+const (
+	HEADER_SIZE = 16 // Size of PageHeader struct: PageID(8) + Checksum(4) + Flags(2) + padding(2)
+)
+
+type Page struct {
+	Header PageHeader
+	Data   [util.PageSize - HEADER_SIZE]byte
+}
+
+type PageHeader struct {
+	PageID   util.PageID
+	Checksum uint32
+	Flags    uint16
+}
