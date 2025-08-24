19d85af6248aaec858ad7247ebc5eb5e7ee5456a
bietrokhonhungvandi <winqti@gmail.com>
2025-08-24 23:23:04 +0700

    chore: initial serialize and deserialize

--- internal/storage/page/page.go
+++ internal/storage/page/page.go
@@ -1,6 +1,8 @@
 package page
 
-import util "github.com/bietkhonhungvandi212/array-db/internal/utils"
+import (
+	util "github.com/bietkhonhungvandi212/array-db/internal/utils"
+)
 
 const (
 	HEADER_SIZE = 16 // Size of PageHeader struct: PageID(8) + Checksum(4) + Flags(2) + padding(2)
@@ -15,4 +17,14 @@ type PageHeader struct {
 	PageID   util.PageID
 	Checksum uint32
 	Flags    uint16
+}
+
+// Serialize packs the page into a byte slice for writing
+func (p *Page) Serialize() []byte {
+	return nil
+}
+
+// Deserialize unpacks from bytes, validates checksum
+func Deserialize(data []byte) (*Page, error) {
+	return nil, nil
 }
