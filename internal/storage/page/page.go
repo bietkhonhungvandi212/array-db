6034cbb09c8353ad0c2ba2c6f32d978f1bd6bd91
bietrokhonhungvandi <winqti@gmail.com>
2025-08-27 07:15:14 +0700

    feat: serialize page

--- internal/storage/page/page.go
+++ internal/storage/page/page.go
@@ -1,6 +1,8 @@
 package page
 
 import (
+	"encoding/binary"
+
 	util "github.com/bietkhonhungvandi212/array-db/internal/utils"
 )
 
@@ -8,19 +10,30 @@ const (
 	HEADER_SIZE = 16 // Size of PageHeader struct: PageID(8) + Checksum(4) + Flags(2) + padding(2)
 )
 
+// Page is block that read/write from disk
 type Page struct {
 	Header PageHeader
 	Data   [util.PageSize - HEADER_SIZE]byte
 }
 
 type PageHeader struct {
-	PageID   util.PageID
-	Checksum uint32
-	Flags    uint16
+	PageID   util.PageID // 8 bytes
+	Checksum uint32      // 4 bytes
+	Flags    uint16      // 2 bytes
+	_        uint16      //2 bytes (padding)
 }
 
 // Serialize packs the page into a byte slice for writing
 func (p *Page) Serialize() []byte {
+	buf := make([]byte, util.PageSize)
+	binary.LittleEndian.PutUint64(buf[0:8], uint64(p.Header.PageID))
+	binary.LittleEndian.PutUint32(buf[8:12], uint32(p.Header.Checksum))
+	binary.LittleEndian.PutUint16(buf[12:14], uint16(p.Header.Flags))
+
+	copy(buf[HEADER_SIZE:], p.Data[:])
+
+	//TODO: Calculate checksum
+
 	return nil
 }
 
