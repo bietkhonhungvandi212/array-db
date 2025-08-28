0002b19bc3574488e03dbbeb405d9120593a80dd
bietrokhonhungvandi <winqti@gmail.com>
2025-08-29 06:31:40 +0700

    feat: setup struct file manager

--- internal/storage/file/file.go
+++ internal/storage/file/file.go
@@ -1,6 +1,12 @@
 package file
 
-import "os"
+import (
+	"os"
+	"sync"
+
+	"github.com/bietkhonhungvandi212/array-db/internal/storage/page"
+	util "github.com/bietkhonhungvandi212/array-db/internal/utils"
+)
 
 /**
 * This module is used to read and write data from / to disk
@@ -8,7 +14,16 @@ import "os"
 **/
 type FileManager struct {
 	file *os.File
+	data []byte
+	size int64
+
+	rwlock sync.Mutex
 }
 
 // When read from disk -> Deseialize the data to page.Page
 // When write to disk -> Serialize the data to []byte and store them in disk by offset
+
+/* READ FILE */
+func (file *FileManager) ReadPage(pageId util.PageID) (*page.Page, error) {
+	return nil, nil
+}
